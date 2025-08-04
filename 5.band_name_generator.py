5c0b725efba6b61a73e680f5322f58a43b84fdba
Bhavesh khullar <bhaveshkhullar9810@gmail.com>
2025-08-04 23:20:48 +0530

    Delete 5.band_name_generator.py

--- 5.band_name_generator.py
+++
@@ -1,4 +0,0 @@
-print("welcome to the band name generator.")
-city = input("which city did you grew up in?\n")
-pet = input("what is the name of your pet?\n")
-print("your band name could be " + city + " " + pet)
