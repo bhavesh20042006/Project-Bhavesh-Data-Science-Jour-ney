a4a0e12bff3e54c7e7abcda7af66119eab784320
Bhavesh khullar <bhaveshkhullar9810@gmail.com>
2025-08-04 23:47:10 +0530

    Add files via upload

---
+++ 3.inputs.py
@@ -0,0 +1,6 @@
+input("what is your name?")
+#input function is used for taking input from the user
+
+print("hello " + input("what is your name?") + "!")
+
+#comment is a line of code or sequence of chracters that the computer ignores
