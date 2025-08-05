a04372ec22ea6b9a4d5f5af2379c0db276e874de
Bhavesh khullar <bhaveshkhullar9810@gmail.com>
2025-08-06 00:40:57 +0530

    Add files via upload

---
+++ Day2_python/12.modulo.py
@@ -0,0 +1,11 @@
+# modulo operator % it gives remainder after deviding two numbers
+print(5 % 2)
+
+
+
+#to check if a number is odd or even
+number = int(input("enter a number :"))
+if number % 2 == 0:
+    print("it is even")
+else:
+    print("it is odd")
