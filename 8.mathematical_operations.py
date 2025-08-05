1d657ade9f44f6d9df5595c74f7024a52d86a474
Bhavesh khullar <bhaveshkhullar9810@gmail.com>
2025-08-05 23:08:21 +0530

    Delete 8.mathematical_operations.py

--- 8.mathematical_operations.py
+++
@@ -1,9 +0,0 @@
-print(1 + 2)   #for addition
-print(2 - 1)   #for subtraction
-print(7 * 3)   #for multiplication
-print(4 / 2)   #for division
-print(5 // 3)  #for devision without decimal
-print(2 ** 3)  #for power
-
-
-#PEMDAS
