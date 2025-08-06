3337c2e5757b1593b2088de39b849e9f862ee557
Bhavesh khullar <bhaveshkhullar9810@gmail.com>
2025-08-06 22:37:32 +0530

    Add files via upload

---
+++ Day2_python/6.data_types.py
@@ -0,0 +1,20 @@
+#strings "hello" , "123"
+print("hello"[0])
+print("hello"[4])
+#pulling out specific chracter from the string is called subscripting
+
+
+
+#integer number without any decimal
+print(123 + 123)
+
+
+
+#floating number decimal numbers
+print(3.14159)
+
+
+
+#boolean
+print(True)
+print(False)
---
+++ Day2_python/8.mathematical_operations.py
@@ -0,0 +1,9 @@
+print(1 + 2)   #for addition
+print(2 - 1)   #for subtraction
+print(7 * 3)   #for multiplication
+print(4 / 2)   #for division
+print(5 // 3)  #for devision without decimal
+print(2 ** 3)  #for power
+
+
+#PEMDAS
