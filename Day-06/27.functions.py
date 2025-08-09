be70ecb9eeaae90efdeb7f112889c6fdcfb1ca3c
Bhavesh khullar <bhaveshkhullar9810@gmail.com>
2025-08-09 23:04:26 +0530

    Add files via upload

---
+++ Day-06/27.functions.py
@@ -0,0 +1,11 @@
+#functions are set of particullar keyboards in a coding language which is used to perform specific tasks
+print("hello")
+num_char = len("hello")
+print(num_char)
+
+#creating own function
+def my_function():
+    print("hello")
+    print("bye")
+
+my_function()
