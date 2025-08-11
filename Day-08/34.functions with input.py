2110b61734f0d78facf9c6bff9f1989d299df0d9
Bhavesh khullar <bhaveshkhullar9810@gmail.com>
2025-08-11 23:35:22 +0530

    Add files via upload

---
+++ Day-08/34.functions with input.py
@@ -0,0 +1,6 @@
+name = input("enter your name: ")
+def greet(name):
+    print("hello " ,name)
+    print("how do you do? " ,name)
+    print("isn't the weather nice? " ,name)
+greet(name)
---
+++ Day-08/35.positional vs keyboard arguments.py
@@ -0,0 +1,7 @@
+def greet_with_name(name,location):
+    print("hello ",name)
+    print("what is it like in: ",location)
+greet_with_name("bhavesh","delhi")
+greet_with_name("delhi","bhavesh")
+greet_with_name(name = "bhavesh",location = "delhi")
+    
