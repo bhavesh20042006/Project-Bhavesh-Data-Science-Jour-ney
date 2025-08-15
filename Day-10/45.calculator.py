4df65f7bc4d281847e8f1e7b101ba2025ebb36e6
Bhavesh khullar <bhaveshkhullar9810@gmail.com>
2025-08-15 23:32:39 +0530

    Add files via upload

---
+++ Day-10/45.calculator.py
@@ -0,0 +1,53 @@
+logo = r"""
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+print(logo)
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+#print(operations["*"](4,8))
+def calculator():
+    should_continue = True
+    num1 = float(input("what is the first number? "))
+    while should_continue:
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("pick an operation: ")
+        num2 = float(input("what is the next number? "))
+        answer = operations[operation_symbol](num1,num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        choice = input("type 'y' to continue or 'n' to exit: ")
+
+        if choice == 'y':
+            num1 = answer
+        else:
+            should_continue = False
+calculator()
